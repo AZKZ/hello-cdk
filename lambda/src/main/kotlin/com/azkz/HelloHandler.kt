16b6a82e7123adf85b0e5f4827a375292a5624c8
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-10-26 07:41:02 +0900

    Lambda関数をAPI Gatewayに対応するように変更した

--- lambda/src/main/kotlin/com/azkz/HelloHandler.kt
+++ lambda/src/main/kotlin/com/azkz/HelloHandler.kt
@@ -1,17 +1,27 @@
 package com.azkz
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
-import java.time.LocalDate
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 
-class HelloHandler:RequestHandler<String,String> {
+class HelloHandler:RequestHandler<APIGatewayProxyRequestEvent,APIGatewayProxyResponseEvent> {
     /**
      * Handles a Lambda Function request
      * @param input The Lambda Function input
      * @param context The Lambda execution environment context object.
      * @return The Lambda Function output
      */
-    override fun handleRequest(input: String?, context: Context?): String {
-        return "Hello World:${input}"
-    }
+    override fun handleRequest(input: APIGatewayProxyRequestEvent?, context: Context?): APIGatewayProxyResponseEvent {
+        val response = APIGatewayProxyResponseEvent()
 
+        if(input == null) {
+            response.statusCode = 400
+            return response
+        }
+
+        response.statusCode = 200
+        response.body = "Hello method:${input.httpMethod}"
+
+        return response
+    }
 }
--- lambda/src/test/kotlin/com/azkz/HelloHandlerTest.kt
+++ lambda/src/test/kotlin/com/azkz/HelloHandlerTest.kt
@@ -1,6 +1,8 @@
 package com.azkz
 
 import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -9,10 +11,14 @@ class HelloHandlerTest {
 
     @Test
     fun testHandleRequest() {
-        val input = "AAAA"
-        val mockContext = mockk<Context>()
-        val returnValue = HelloHandler().handleRequest(input,mockContext)
+        val method = "GET"
+        val mockInput = mockk<APIGatewayProxyRequestEvent>()
 
-        assertEquals("Hello World:${input}",returnValue)
+        every { mockInput.httpMethod } returns method
+
+        val mockContext = mockk<Context>()
+        val returnValue = HelloHandler().handleRequest(mockInput,mockContext)
+
+        assertEquals("Hello method:${method}",returnValue.body)
     }
 }
