dad139a1ed655d22770c865e0e08e1e876606a4e
kazuya azegami <aze.gk.hh.esp@gmail.com>
2020-10-26 07:00:12 +0900

    API設定の追加

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,5 +1,7 @@
 import * as core from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigateway from "@aws-cdk/aws-apigateway";
+
 
 export class HelloCdkStack extends core.Stack {
   constructor(scope: core.Construct, id: string, props?: core.StackProps) {
@@ -7,10 +9,17 @@ export class HelloCdkStack extends core.Stack {
     console.log(__dirname)
 
     // Lambda関数
-    const hello = new lambda.Function(this, 'HelloHandler', {
+    const helloHandler = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.JAVA_11,
       code: lambda.Code.fromAsset('lambda/build/distributions/lambda-1.0-SNAPSHOT.zip'),
       handler: 'com.azkz.HelloHandler'
     })
+
+    // API
+    const helloApi = new apigateway.LambdaRestApi(this,'hello-api',{
+      handler:helloHandler,
+    })
+    helloApi.root.addMethod('GET');
+    helloApi.root.addMethod('POST');
   }
 }
